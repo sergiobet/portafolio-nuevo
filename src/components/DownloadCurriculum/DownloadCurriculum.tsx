b6c0d567ef29bf7290395166aee70799d3e7e7c8
Sergio Betancourt <45174963+sergiobet@users.noreply.github.com>
2025-07-29 15:48:21 -0400

    Se modifican clases del boton para cambiar estilos

--- src/components/DownloadCurriculum/DownloadCurriculum.tsx
+++ src/components/DownloadCurriculum/DownloadCurriculum.tsx
@@ -1,6 +1,6 @@
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 
-function DownloadCurriculum() {
+const DownloadCurriculum = () => {
   const handleDownload = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
     const link = document.createElement("a");
@@ -15,7 +15,7 @@ function DownloadCurriculum() {
   return (
     <button
       onClick={handleDownload}
-      className="inline-flex text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-800 hover:text-white focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:text-white! dark:border-gray-600 dark:hover:bg-gray-700! dark:hover:border-gray-600 cursor-pointer dark:text-white w-full mx-auto justify-center md:justify-start md:w-auto animate-pingOnce"
+      className="inline-flex text-gray-900 bg-gray-800 text-white border border-gray-300 focus:outline-none hover:bg-white hover:text-black focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:text-white! dark:border-gray-600 dark:hover:bg-gray-700! dark:hover:border-gray-600 cursor-pointer dark:text-white w-full mx-auto justify-center md:justify-start md:w-auto animate-pingOnce"
       aria-label="Descargar CV"
     >
       Descargar CV
