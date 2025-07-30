a466b118fb6bfadc762bcdb3ead09a714aff2be8
Sergio Betancourt <45174963+sergiobet@users.noreply.github.com>
2025-07-30 12:47:01 -0400

    Se realiza modificación en nombre de propiedad que es buscado en la data

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -4,25 +4,26 @@ interface SkillsProps {
   data: PortfolioData;
 }
 const Skills: React.FC<SkillsProps> = ({ data }) => {
-  const skillGroups = data?.skillGroups ?? [];
-  
+  const skillGroups = data?.skills ?? [];
+
   return (
     <section id="habilidades">
-        <br /><br /><br /><br />
+      
       <h1 className="text-4xl font-semibold mb-6 text-left">
         Habilidades Técnicas
       </h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 animate-fadeInOnce">
-        
         {skillGroups.map((group, i) => (
           <div
             key={i}
             className="border border-black dark:border-white rounded-lg p-6 shadow-md"
           >
-            <h3 className="text-xl font-semibold mb-4 text-left">{group.title}</h3>
-            <div className="space-x-1 text-left">
+            <h3 className="text-xl font-semibold mb-4 text-left">
+              {group.title}
+            </h3>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 text-left">
               {group.technologies.map((technology, index) => (
-                <span key={index} className="text-base text-left">
+                <span key={index} className="text-base">
                   • {technology}
                 </span>
               ))}
