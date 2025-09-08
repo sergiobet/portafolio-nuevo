2d43a6a9c3602e180cf4994ecf3a06ad9f388555
sergiobet <sbetancourt776@gmail.com>
2025-09-08 19:58:32 -0400

    Updates

--- jest.config.ts
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: "jsdom",
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
-  },
-  setupFilesAfterEnv: ['@testing-library/jest-dom'],
-};
